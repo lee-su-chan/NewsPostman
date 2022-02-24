78c3e3e3e3b6a685166692ec1811643566f9e069
이수찬 <dodopa@frientrip.com>
2022-02-24 22:58:56 +0900

    app 모듈의 build.gradle 종속성 상수 사용하도록 수정

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -14,4 +14,16 @@ object Dep {
         private const val version = "1.5.31"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     }
+
+    object Android {
+        const val core = "androidx.core:core-ktx:1.7.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.4.1"
+        const val material = "com.google.android.material:material:1.5.0"
+    }
+
+    object Test {
+        const val junit = "junit:junit:4.13.2"
+        const val junitExt = "androidx.test.ext:junit:1.1.3"
+        const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+    }
 }
