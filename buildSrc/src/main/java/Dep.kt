6d192065edb05334451ce0a9a7ce564dca51f0f6
이수찬 <dodopa@frientrip.com>
2022-02-24 22:55:17 +0900

    프로젝트 범위 gradle에서 종속성 상수화

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -5,3 +5,13 @@ object Version {
     const val versionCode = 1
     const val versionName = "1.0.0"
 }
+
+object Dep {
+
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.4"
+
+    object Kotlin {
+        private const val version = "1.5.31"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+    }
+}
