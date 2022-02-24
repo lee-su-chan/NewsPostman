d3544d8a5a9d8b122d4d9d4ef34176c3e775d2a8
이수찬 <dodopa@frientrip.com>
2022-02-24 23:30:54 +0900

    core-ui 모듈에 BaseActivity 추가

---
+++ features/core-ui/src/main/java/com/leesuchan/coreui/ui/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.leesuchan.coreui.ui
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes
+    private val layoutRes: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+    }
+}
