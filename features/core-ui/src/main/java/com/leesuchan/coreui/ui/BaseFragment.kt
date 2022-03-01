8486c7ce875828842b35d7b0586e8522f0263d2e
이수찬 <dodopa@frientrip.com>
2022-03-01 21:19:45 +0900

    base 모듈에 BaseFragment 추가

---
+++ features/core-ui/src/main/java/com/leesuchan/coreui/ui/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.leesuchan.coreui.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    private val layoutRes: Int
+) : Fragment() {
+
+    protected lateinit var binding: B
+
+    protected abstract val viewModel: BaseViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+}
