28b88e4d03fd6ecc59c133854463ecd16fb5b536
이수찬 <dodopa@frientrip.com>
2022-03-01 20:39:48 +0900

    app 모듈에 임시로 MainActiivty 추가

---
+++ app/src/main/java/com/leesuchan/newspostman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.leesuchan.newspostman
+
+import com.leesuchan.coreui.ui.BaseActivity
+import com.leesuchan.newspostman.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
