8d13822a336b4d915c06213942ca4a3cb1759c3a
이수찬 <dodopa@frientrip.com>
2022-03-01 20:58:13 +0900

    필요없는 MainActivity 제거

--- app/src/main/java/com/leesuchan/newspostman/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.leesuchan.newspostman
-
-import com.leesuchan.coreui.ui.BaseActivity
-import com.leesuchan.newspostman.databinding.ActivityMainBinding
-
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
