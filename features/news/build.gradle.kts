e89f6e63400af241f0d161b372b5998269c50b26
이수찬 <dodopa@frientrip.com>
2022-03-01 21:16:08 +0900

    NewsNavHostActivity에 Hilt 적용

--- features/news/build.gradle.kts
+++ features/news/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -16,4 +17,8 @@ dependencies {
     implementation(Dep.Android.core)
     implementation(Dep.Android.appcompat)
     implementation(Dep.Android.material)
+    implementation(Dep.Android.activity)
+
+    implementation(Dep.Dagger.hiltAndroid)
+    kapt(Dep.Dagger.hiltCompiler)
 }
--- features/news/src/main/java/com/leesuchan/news/NewsNavHostActivity.kt
+++ features/news/src/main/java/com/leesuchan/news/NewsNavHostActivity.kt
@@ -1,7 +1,14 @@
 package com.leesuchan.news
 
+import androidx.activity.viewModels
 import com.leesuchan.coreui.ui.BaseActivity
+import com.leesuchan.coreui.ui.EmptyViewModel
 import com.leesuchan.news.databinding.ActivityNewsNavHostBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NewsNavHostActivity :
-    BaseActivity<ActivityNewsNavHostBinding>(R.layout.activity_news_nav_host)
+    BaseActivity<ActivityNewsNavHostBinding>(R.layout.activity_news_nav_host) {
+
+    override val viewModel: EmptyViewModel by viewModels()
+}
