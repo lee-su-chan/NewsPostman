75b13b438a7c8629e2d565e84e0683e66e2e6cc0
Lee SuChan <dopa_1@naver.com>
2022-03-02 00:18:20 +0900

    data 모듈에 NetworkModule 추가

---
+++ data/src/main/java/com/leesuchan/data/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.leesuchan.data.di
+
+import android.content.Context
+import com.prof.rssparser.Parser
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRssParser(@ApplicationContext context: Context): Parser =
+        Parser.Builder().context(context).build()
+}
