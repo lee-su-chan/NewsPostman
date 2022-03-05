6d04a99a27689ffa0ba4e8abe9f836e27cca0ebc
Lee SuChan <dopa_1@naver.com>
2022-03-05 23:40:21 +0900

    NewsDao에 getNewsList() 함수 추가

--- data/src/main/java/com/leesuchan/data/news/local/NewsDao.kt
+++ data/src/main/java/com/leesuchan/data/news/local/NewsDao.kt
@@ -3,11 +3,16 @@ package com.leesuchan.data.news.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.leesuchan.data.news.model.ListingNewsEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NewsDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNewsList(newsList: List<ListingNewsEntity>)
+
+    @Query("SELECT * FROM ListingNewsEntity ORDER BY pub_date")
+    fun getNewsList(): Flow<List<ListingNewsEntity>>
 }
