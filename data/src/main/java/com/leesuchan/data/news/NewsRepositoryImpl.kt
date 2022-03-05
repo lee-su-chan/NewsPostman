cdab00799e3d242a3d0f4b357697048cb80e3199
Lee SuChan <dopa_1@naver.com>
2022-03-05 23:29:46 +0900

    NewsRepositoryImpl의 InsertListingNews() 함수 구현

--- data/src/main/java/com/leesuchan/data/news/NewsRepositoryImpl.kt
+++ data/src/main/java/com/leesuchan/data/news/NewsRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.leesuchan.data.news
 
+import com.leesuchan.data.news.local.NewsDao
 import com.leesuchan.data.news.model.ListingNewsEntity
 import javax.inject.Inject
 
-class NewsRepositoryImpl @Inject constructor() : NewsRepository {
+class NewsRepositoryImpl @Inject constructor(
+    private val newsDao: NewsDao
+) : NewsRepository {
 
     override suspend fun insertListingNews(news: List<ListingNewsEntity>) {
-        // TODO : 로컬 DB에 뉴스 목록 삽입 로직 추가
+        newsDao.insertNewsList(news)
     }
 }
