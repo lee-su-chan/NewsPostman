fe6ef60caffe01ea0623c9ff20e6b337e3789c01
Lee SuChan <dopa_1@naver.com>
2022-03-05 17:14:28 +0900

    NewsDao에 insertNewsList() 함수 추가

--- data/src/main/java/com/leesuchan/data/news/local/NewsDao.kt
+++ data/src/main/java/com/leesuchan/data/news/local/NewsDao.kt
@@ -1,8 +1,13 @@
 package com.leesuchan.data.news.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.leesuchan.data.news.model.ListingNewsEntity
 
 @Dao
 interface NewsDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNewsList(newsList: List<ListingNewsEntity>)
 }
