0fb01f6d5b353f8efd570f4174b741a5c2467a72
Lee SuChan <dopa_1@naver.com>
2022-03-05 16:38:47 +0900

    NewsPostmanDatabase에 NewsDao 추가

--- data/src/main/java/com/leesuchan/data/NewsPostmanDatabase.kt
+++ data/src/main/java/com/leesuchan/data/NewsPostmanDatabase.kt
@@ -4,10 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.leesuchan.data.news.local.DateConverter
+import com.leesuchan.data.news.local.NewsDao
 import com.leesuchan.data.news.model.ListingNewsEntity
 
 @Database(entities = [ListingNewsEntity::class], version = 1)
 @TypeConverters(DateConverter::class)
 abstract class NewsPostmanDatabase : RoomDatabase() {
 
+    abstract fun newsDao(): NewsDao
 }
---
+++ data/src/main/java/com/leesuchan/data/news/local/NewsDao.kt
@@ -0,0 +1,8 @@
+package com.leesuchan.data.news.local
+
+import androidx.room.Dao
+
+@Dao
+interface NewsDao {
+
+}
