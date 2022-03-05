28be76772f392bcc0c0153ed52739d37f7d60d62
Lee SuChan <dopa_1@naver.com>
2022-03-05 17:14:01 +0900

    NewsPostmanDatabase에 instance 생성 로직 추가

--- data/src/main/java/com/leesuchan/data/NewsPostmanDatabase.kt
+++ data/src/main/java/com/leesuchan/data/NewsPostmanDatabase.kt
@@ -1,6 +1,8 @@
 package com.leesuchan.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.leesuchan.data.news.local.DateConverter
@@ -12,4 +14,22 @@ import com.leesuchan.data.news.model.ListingNewsEntity
 abstract class NewsPostmanDatabase : RoomDatabase() {
 
     abstract fun newsDao(): NewsDao
+
+    companion object {
+        @Volatile private var instance: NewsPostmanDatabase? = null
+
+        fun getInstance(context: Context): NewsPostmanDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): NewsPostmanDatabase {
+            return Room.databaseBuilder(
+                context,
+                NewsPostmanDatabase::class.java,
+                "NewsPostmanDatabase"
+            ).build()
+        }
+    }
 }
