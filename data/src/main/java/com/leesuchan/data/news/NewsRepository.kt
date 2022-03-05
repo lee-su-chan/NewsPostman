4418cdc522d98b76f6e8470e043a2e8309690d6b
Lee SuChan <dopa_1@naver.com>
2022-03-05 23:40:37 +0900

    NewsRepository, NewsRepositoryImpl에 newsList 프로퍼티 추가

--- data/src/main/java/com/leesuchan/data/news/NewsRepository.kt
+++ data/src/main/java/com/leesuchan/data/news/NewsRepository.kt
@@ -1,8 +1,11 @@
 package com.leesuchan.data.news
 
 import com.leesuchan.data.news.model.ListingNewsEntity
+import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
+
+    val newsList: Flow<List<ListingNewsEntity>>
 
     suspend fun insertListingNews(news: List<ListingNewsEntity>)
 }
--- data/src/main/java/com/leesuchan/data/news/NewsRepositoryImpl.kt
+++ data/src/main/java/com/leesuchan/data/news/NewsRepositoryImpl.kt
@@ -2,11 +2,15 @@ package com.leesuchan.data.news
 
 import com.leesuchan.data.news.local.NewsDao
 import com.leesuchan.data.news.model.ListingNewsEntity
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
     private val newsDao: NewsDao
 ) : NewsRepository {
+
+    override val newsList: Flow<List<ListingNewsEntity>>
+        get() = newsDao.getNewsList()
 
     override suspend fun insertListingNews(news: List<ListingNewsEntity>) {
         newsDao.insertNewsList(news)
