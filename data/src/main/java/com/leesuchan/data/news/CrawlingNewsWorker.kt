99b7cb0dcd087dea6a9be0f9049ca1d77c5de664
Lee SuChan <dopa_1@naver.com>
2022-03-05 16:06:29 +0900

    CrawlingNewsWorker hilt 적용 및 inputData를 기반으로 크롤링하도록 수정

--- data/src/main/java/com/leesuchan/data/news/CrawlingNewsWorker.kt
+++ data/src/main/java/com/leesuchan/data/news/CrawlingNewsWorker.kt
@@ -1,21 +1,38 @@
 package com.leesuchan.data.news
 
 import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.prof.rssparser.Parser
-import dagger.hilt.android.AndroidEntryPoint
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import timber.log.Timber
 import javax.inject.Inject
 
-@AndroidEntryPoint
-class CrawlingNewsWorker(appContext: Context, workerParams: WorkerParameters) :
-    CoroutineWorker(appContext, workerParams) {
+@HiltWorker
+class CrawlingNewsWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters
+) : CoroutineWorker(appContext, workerParams) {
+
+    companion object {
+        private const val KEY_CRAWLING_URL = "key_crawling_url"
+
+        fun createInputData(crawlingUrl: String): Data = workDataOf(KEY_CRAWLING_URL to crawlingUrl)
+    }
 
     @Inject lateinit var parser: Parser
 
     override suspend fun doWork(): Result {
-        val channel = parser.getChannel("")
+        val crawlingUrl = inputData.getString(KEY_CRAWLING_URL) ?: return Result.failure()
+        val channel = parser.getChannel(crawlingUrl)
+
         // TODO : NewsRepository에서 뉴스 목록 삽입 함수 호출
+        Timber.d("channel: $channel")
+
         return Result.success()
     }
 }
