a11f37c8cb54d8da26fca8de1d9930fc6d44cd97
Lee SuChan <dopa_1@naver.com>
2022-03-05 23:31:16 +0900

    CrawlingNewsWorker에서 크롤링된 데이터 데이터모델로 맵핑 후 Repository에 적재하는 로직 추가

--- data/src/main/java/com/leesuchan/data/news/CrawlingNewsWorker.kt
+++ data/src/main/java/com/leesuchan/data/news/CrawlingNewsWorker.kt
@@ -6,10 +6,14 @@ import androidx.work.CoroutineWorker
 import androidx.work.Data
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
+import com.leesuchan.data.news.model.ListingNewsEntity
+import com.prof.rssparser.Article
 import com.prof.rssparser.Parser
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltWorker
@@ -25,14 +29,45 @@ class CrawlingNewsWorker @AssistedInject constructor(
     }
 
     @Inject lateinit var parser: Parser
+    @Inject lateinit var newsRepository: NewsRepository
 
     override suspend fun doWork(): Result {
-        val crawlingUrl = inputData.getString(KEY_CRAWLING_URL) ?: return Result.failure()
-        val channel = parser.getChannel(crawlingUrl)
+        try {
+            val crawlingUrl = inputData.getString(KEY_CRAWLING_URL) ?: return Result.failure()
+            val channel = parser.getChannel(crawlingUrl)
 
-        // TODO : NewsRepository에서 뉴스 목록 삽입 함수 호출
-        Timber.d("channel: $channel")
+            val newsList = channel.articles.toListingNewsEntityList()
+            Timber.d("newsList: $newsList")
 
-        return Result.success()
+            newsRepository.insertListingNews(newsList)
+
+            return Result.success()
+        } catch (e: Exception) {
+            Timber.e(e)
+            return Result.failure()
+        }
     }
+}
+
+fun List<Article>.toListingNewsEntityList(): List<ListingNewsEntity> =
+    asSequence().map { it.toListingNewsEntity() }.filterNotNull().toList()
+
+fun Article.toListingNewsEntity(): ListingNewsEntity? {
+    val id = guid ?: return null
+    val publishDateStr = pubDate ?: ""
+    val publishDate = if (publishDateStr.isBlank()) Date() else {
+        val sourceSdf = SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+        val result = sourceSdf.parse(publishDateStr)
+        result
+    }
+    val authorName = sourceName ?: ""
+    val newsTitle = title?.removeSuffix(" - $authorName") ?: ""
+
+    return ListingNewsEntity(
+        id = id,
+        title = newsTitle,
+        pubDate = publishDate,
+        authorName = authorName,
+        link = link ?: ""
+    )
 }
